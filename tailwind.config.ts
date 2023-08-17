41f6085d641940b565e874855b5598ee5d1fe683
Andeilson Ferreira <thugmansion@Andeilsons-MacBook-Pro.local>
2023-08-17 12:02:33 -0300

    created project with next 13 and added tailwindcss dark mode theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,40 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: {
+          100: '#0D0D0D',
+          200: '#181818',
+        },
+        gray: {
+          100: '#C5C5C5',
+          200: 'rgba(128, 128, 128, 0.5)',
+          300: '#808080',
+          400: '#383737',
+        },
+        white: '#ffffff',
+        blue: 'rgba(63, 100, 234, 0.43)',
+        red: 'rgba(233, 63, 64, 0.43)',
+        yellow: 'rgba(255, 184, 0, 0.43)',
+      },
+      fontFamily: {
+        sans: 'var(--font-inter)',
+      },
+      fontSize: {
+        '10': '0.625rem',
+        '11': '0.6875rem',
+        '12': '0.75rem',
+        '14': '0.875rem',
+        '15': '0.9375rem',
+        '16': '1rem',
+        '18': '1.125rem',
+        '20': '1.25rem',
+        '24': '1.5rem',
+        '32': '2rem',
+        '40': '2.5rem',
+        '48': '3rem',
+        '60': '3.75rem',
+        '80': '5rem',
       },
     },
   },
